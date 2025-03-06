f81100dcbddc9becdae70b6ba8c2e2c75a4c9082
Harshit Mishra <117696951+Harshit2534@users.noreply.github.com>
2025-03-06 11:49:48 +0530

    Update Pallindrome_String.java

--- Pallindrome_String.java
+++ Pallindrome_String.java
@@ -2,15 +2,31 @@ import java.util.*;
 public class Pallindrome_String {
 
     public static void main(String[] args) {
+        // String s = "abcddcba";
+        // char[] s2 = s.toCharArray();
+        // int n = s.length();
+        // char[] s1 = new char[n];
+        // for (int i = 0; i < n; i++) {
+        //     s1[i] = s2[n - i - 1];
+        // }
+
+        // if (Arrays.equals(s1, s2)) {
+        //     System.out.println("It is a palindrome");
+        // } else {
+        //     System.out.println("It is not a palindrome");
+        // }
         String s = "abcddcba";
-        char[] s2 = s.toCharArray();
         int n = s.length();
-        char[] s1 = new char[n];
-        for (int i = 0; i < n; i++) {
-            s1[i] = s2[n - i - 1];
+        boolean isPalindrome = true; 
+
+        for (int i = 0; i < n / 2; i++) {
+            if (s.charAt(i) != s.charAt(n - 1 - i)) {
+                isPalindrome = false; 
+                break; 
+            }
         }
 
-        if (Arrays.equals(s1, s2)) {
+        if (isPalindrome) {
             System.out.println("It is a palindrome");
         } else {
             System.out.println("It is not a palindrome");
